177367b98185d53d8d18f9e74ec35acda420e572
Andrey Mironov <andrey@MacBook-Pro-Andrey.local>
2020-10-27 15:58:25 +0300

    process

--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,22 @@
-// let arrow = document.querySelector('.next');
-// let prev = document.querySelector('.prev');
+let btn = document.querySelector('.ham');
+let menu = document.querySelector('.menu');
 
-$(document).ready(function () {
-   $('.main-slider').slick({
-      dots: true,
-      infinite: true,
-      adaptiveHeight: true,
-   });
+
+btn.addEventListener('click', function() {  
+   headerMenu();
 });
 
-let btn = document.querySelector('.header__burger');
-let menu = document.querySelector('.menu__list');
-let links = document.querySelector('.menu__item');
-let social = document.querySelector('.header__social');
-let adress = document.querySelector('.header__metro');
-let menuLink = document.querySelector('.menu__link-add');
-let menuAdd = document.querySelector('.menu__add');
-
-btn.addEventListener('click', function () {
-   toogleMenu();
-   btnclose();
-});
-
-function toogleMenu() {
+function headerMenu() {
+   btn.classList.toggle('active');
    menu.classList.toggle('active');
 }
 
-let btnclose = function () {
-   if (menu.classList.contains('active')) {
-      btn.classList.add('active-btn');
-   } else {
-      btn.classList.remove('active-btn');
-   }
-};
-
-
-// menuLink.addEventListener('click', function (evt) {
-//    evt.preventDefault();
-//    menuAdd.classList.toggle('menu__add-active');
-// });
+$(document).ready(function(){
+  $('.slider').slick({
+   dots: true,
+   infinite: true,
+   speed: 500,
+   fade: true,
+   cssEase: 'linear'
+  });
+});
